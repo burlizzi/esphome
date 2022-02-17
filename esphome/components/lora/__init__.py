a4ccc39694db8d3f15df319222a107678fff8244
Luca Burlizzi <burlizzi@gmail.com>
2022-02-17 13:23:18 +0100

    lora working

--- esphome/components/lora/__init__.py
+++ esphome/components/lora/__init__.py
@@ -1,4 +1,5 @@
 
+from typing import Literal
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
@@ -15,6 +16,9 @@ CONF_DIO1 = "dio1_pin"
 CONF_DIO2 = "dio2_pin"
 CONF_DIO5 = "dio5_pin"
 CONF_DATA_ACTION = "data_action"
+CONF_LORA_CLASS = "class"
+CONF_LORA_FREQ = "FREQUENCY_PLAN"
+
 
 DEPENDENCIES = ["spi"]
 
@@ -23,21 +27,45 @@ ns = lora_ns = cg.esphome_ns.namespace("lora")
 Lora = ns.class_("Lora",cg.Component)
 
 
+LORA_CLASS_ENUM = cg.global_ns.enum("devclass_t")
+
+LORA_CLASS = {
+    'CLASS_A': LORA_CLASS_ENUM.CLASS_A,
+    'CLASS_C': LORA_CLASS_ENUM.CLASS_C,
+}
+
+RX2DR_ENUM = cg.global_ns.enum("dataRates_t")
+
+RX2DR = {
+    'SF9': RX2DR_ENUM.SF9BW125,
+    'SF12': RX2DR_ENUM.SF12BW125,
+}
+
+
+
+
+
 #MULTI_CONF = True
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(Lora),
 
         cv.Required(CONF_BAND): cv.one_of('EU_868',"AS_923","US_915","AU_915"),
-        cv.Required(CONF_CS_PIN): pins.internal_gpio_output_pin_number,
         cv.Required(CONF_DEVEUI): cv.string,
         cv.Optional(CONF_APPEUI,"0000000000000000"): cv.string,
         cv.Required(CONF_APPKEY): cv.string,
-        cv.Required(CONF_RESET): pins.internal_gpio_output_pin_number,
-        cv.Required(CONF_DIO0): pins.internal_gpio_output_pin_number,
-        cv.Required(CONF_DIO1): pins.internal_gpio_input_pin_number,
-        cv.Required(CONF_DIO2): pins.internal_gpio_input_pin_number,
-        cv.Optional(CONF_DIO5,0): pins.internal_gpio_input_pin_number,
+
+        cv.Optional(CONF_LORA_CLASS,'CLASS_C'): cv.enum(LORA_CLASS),
+        cv.Optional(CONF_LORA_FREQ,'SF12'): cv.enum(RX2DR),
+
+
+        cv.Optional(CONF_CS_PIN,'SS'): pins.internal_gpio_output_pin_number,
+        cv.Optional(CONF_RESET,'RST_LoRa'): pins.internal_gpio_output_pin_number,
+        cv.Optional(CONF_DIO0,'DIO0'): pins.internal_gpio_output_pin_number,
+        cv.Optional(CONF_DIO1,'DIO1'): pins.internal_gpio_input_pin_number,
+        cv.Optional(CONF_DIO2,'DIO2'): pins.internal_gpio_input_pin_number,
+        cv.Optional(CONF_DIO5): pins.internal_gpio_input_pin_number,
+
         cv.Optional(CONF_DATA_ACTION): automation.validate_automation(
             single=True
         ),        
@@ -47,15 +75,19 @@ CONFIG_SCHEMA = cv.Schema(
 
 
 async def to_code(config):
-    cg.add_library("https://github.com/BeelanMX/Beelan-LoRaWAN", "2.0.0")
+    #cg.add_library("https://github.com/burlizzi/Beelan-LoRaWAN", "")
+    #cg.add_library("git:../../../Beelan-LoRaWAN", "")
     cg.add_build_flag("-D"+config[CONF_BAND])
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     cg.add(var.setDevEUI(config[CONF_DEVEUI]))
     cg.add(var.setAppEUI(config[CONF_APPEUI]))
     cg.add(var.setAppKey(config[CONF_APPKEY]))
+    cg.add(var.setDeviceClass(config[CONF_LORA_CLASS]))
+    cg.add(var.setRX2DR(config[CONF_LORA_FREQ]))
+    
     if CONF_DATA_ACTION in config:
         await automation.build_automation(
-            var.data_trigger(), [(int, "port"),(cg.std_string, "data")], config[CONF_DATA_ACTION]
+            var.data_trigger(), [(int, "port"),(int, "rssi"),(cg.std_string, "data")], config[CONF_DATA_ACTION]
         )
-    cg.add_global(cg.RawStatement(f'const sRFM_pins RFM_pins = {{.CS={config[CONF_CS_PIN]},.RST={config[CONF_RESET]},.DIO0={config[CONF_DIO0]},.DIO1={config[CONF_DIO1]},.DIO2={config[CONF_DIO2]},.DIO5={config[CONF_DIO5]},}};'))
+    cg.add_global(cg.RawStatement(f'const sRFM_pins RFM_pins = {{.CS={config[CONF_CS_PIN]},.RST={config[CONF_RESET]},.DIO0={config[CONF_DIO0]},.DIO1={config[CONF_DIO1]},.DIO2={config[CONF_DIO2]}}};'))
--- esphome/components/lora/lora.h
+++ esphome/components/lora/lora.h
@@ -7,72 +7,144 @@
 
 #include <lorawan.h>
 
+
+
+void xxx(const char *format, ...) {  // NOLINT
+  va_list arg;
+  va_start(arg, format);
+  
+  esphome::esp_log_vprintf_(ESPHOME_LOG_LEVEL_ERROR, "lora", 22, format, arg);
+  va_end(arg);
+}
+
+
+
 namespace esphome{
     namespace lora{
         class Lora : public Component, public LoRaWANClass, public Controller
         {
-            static const uint32_t RESTORE_STATE_VERSION = 0x2a5a2c10UL;
-
             public:
+            bool joined;
+
+            Lora():joined(false)
+            {
+                if(!init()){
+                  ESP_LOGE("lora","RFM95 not detected");
+                }
+            }
             void setup() override
             {
+                setup_controller();
                 uint32_t* dev=(uint32_t*)DevEUI;
-                auto rtc = global_preferences->make_preference<uint32_t>(RESTORE_STATE_VERSION^dev[0]^dev[1]);
-                if(!rtc.load(&Session_Data))
+                rtc = global_preferences->make_preference<sLoRa_Session>(RESTORE_STATE_VERSION^dev[0]^dev[1],true);
+            }
+
+
+
+            void start()
+            {
+
+                setTxPower(20,PA_BOOST_PIN);
+                auto loaded=rtc.load(&Session_Data);
+                ESP_LOGI("lora","store... load=%d devaddr=%d",loaded,*(uint32_t*)Session_Data.DevAddr);
+                if(!loaded || ((*(uint32_t*)Session_Data.DevAddr)==0))
                 {
-                    bool isJoined=false;;
                     ESP_LOGI("lora","Joining...");
-                    for (size_t rate = SF7BW125; rate >=SF12BW125 && !isJoined; rate--)
+                    for (size_t rate = SF9BW125; (rate >=SF12BW125) && !joined; rate--)
                     {
                         setDataRate(rate);
                         ESP_LOGI("lora","SF=%d",12-rate);
-                        for (size_t i = CH0; i < CH3 && !isJoined; i++)
+                        for (size_t i = CH0; i < CH3 && !joined; i++)
                         {
-                        setChannel(i);
-                        isJoined = join();
-                        if (!isJoined)
-                        {
-                            delay(10000);
 
-                        }
+                          setChannel(i);
+                          ESP_LOGI("lora","CH=%d",i);
+                          disableLoopWDT();
+                          joined = join();
+                          enableLoopWDT();
+                          
+                          if (!joined)
+                          {
+                            ESP_LOGI("lora","failed, wait 10 sec");
+                            for (size_t z = 0; z < 10; z++)
+                            {
+                              App.feed_wdt();
+                              delay(1000);
+                              yield();
+                            }
+                          }
                         }
                     }
-                    if (!isJoined)
+                    if (!joined)
                     {
                         ESP_LOGE("lora","cannot join");
+                        //mark_failed();
                         return;
                     }
                     rtc.save(&Session_Data);
+                    
                 }
                 else
                 {
                     ESP_LOGI("lora","using saved session");
+                    joined = true;
+
                 }
-                setup_controller();
+                setChannel(MULTI);              
+                setDataRate(SF9BW125);
             }
+
+            void setRX2DR(dataRates_t dr)
+            {
+                ESP_LOGI("lora","OBSOLETE AND DEPRECATED SX2 data rate=%d",12-dr);
+                //LoRa_Settings.RX2_Datarate_Rx=dr;
+            }
+
+            void reset()
+            {
+              *(uint32_t*)Session_Data.DevAddr=0;
+              rtc.save(&Session_Data);
+            }
+            void send(String message="ciao")
+            {
+              if (joined)
+              {
+                sendUplink((char*)message.c_str(), message.length(), 0, 1);
+                ESP_LOGI("lora","sent on channel %d",LoRa_Settings.Channel_Tx);
+                rtc.save(&Session_Data);
+              }
+              else
+              {
+                ESP_LOGE("lora","not joined");
+              }
+            }
+
+
             void loop() override
             {
                 char outStr[255];
                 update();
                 auto recvStatus = readData(outStr);
                 if(recvStatus) {
-                    ESP_LOGI("lora","====>>(%d) %s",Message_Rx.Frame_Port, outStr);
+                    ESP_LOGI("lora","====>>(%d) rssi(%d) %s",Message_Rx.Frame_Port, getRssi(), outStr);
                     if(data_trigger_)
-                        data_trigger_->trigger(Message_Rx.Frame_Port,std::string(outStr,recvStatus));
+                        data_trigger_->trigger(Message_Rx.Frame_Port,getRssi(),std::string(outStr,recvStatus));
                 }
             }
 
-            Trigger<int,std::string> *data_trigger() {
+            Trigger<int,int,std::string> *data_trigger() {
                 if (!this->data_trigger_)
-                this->data_trigger_ = make_unique<Trigger<int,std::string>>();
+                this->data_trigger_ = make_unique<Trigger<int,int,std::string>>();
                 return this->data_trigger_.get();
             }
             protected:
-            std::unique_ptr<Trigger<int,std::string>> data_trigger_{nullptr};
+            std::unique_ptr<Trigger<int,int,std::string>> data_trigger_{nullptr};
+            static const uint32_t RESTORE_STATE_VERSION = 0x2a5a2c13UL;
+            ESPPreferenceObject rtc;
 
-        };
 
-        #ifdef USE_BINARY_SENSOR
+
+#ifdef USE_BINARY_SENSOR
   void on_binary_sensor_update(binary_sensor::BinarySensor *obj, bool state) override;
 #endif
 #ifdef USE_COVER
@@ -88,7 +160,7 @@ namespace esphome{
   void on_sensor_update(sensor::Sensor *obj, float state) override;
 #endif
 #ifdef USE_SWITCH
-  void on_switch_update(switch_::Switch *obj, bool state) override;
+  void on_switch_update(switch_::Switch *obj, bool state) override {ESP_LOGE("lora","not implemented");}
 #endif
 #ifdef USE_TEXT_SENSOR
   void on_text_sensor_update(text_sensor::TextSensor *obj, const std::string &state) override;
@@ -101,7 +173,10 @@ namespace esphome{
 #endif
 #ifdef USE_SELECT
   void on_select_update(select::Select *obj, const std::string &state) override;
-#endif
+#endif            
+
+        };
+
 
     }
 }
